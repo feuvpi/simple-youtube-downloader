893533235497752b2b168cf472c44d4cab4d6a5c
feuvpi <fredvpg@gmail.com>
2023-01-11 00:02:48 -0300

    implementing frame and elements.

--- main.py
+++ main.py
@@ -14,10 +14,30 @@ frame = customtkinter.CTkFrame(master=root)
 # build a simple login system
 root = tk.CTk()
 root.geometry("500x350")
+frame.pack(pady=20, padx=2, fill="both", expand=True)
 root.title("Simple YouTube Downloader")
 
-def login():
-    link = entry1.get
+# adding label
+label = tk.CTkLabel(master=frame, text="Youtube Downloader", font=("Roboto", 24))
+label.pack(pady=12, padx=4)
+
+# adding text field for input link
+link_field = tk.CTkEntry(master=frame, width=400, placeholder_text="Enter video url:")
+link_field.pack(pady=2, padx=2)
+
+# adding dropdown to select the file type
+combobox_var = tk.StringVar(value="MP3")  # set initial value
+type_field = tk.CTkComboBox(master=frame, values=['MP4', 'MP3'], variable='combobox_var', width=400)
+type_field['values'] = ('MP4', 'MP3')
+type_field.configure(values=['MP4', 'MP3'], variable='combobox_var')
+type_field.pack(pady=12, padx=10)
+
+# adding download button
+button = tk.CTkButton(master=frame, width=400, text="Download", command=download)
+button.pack(pady=12, padx=10)
+
+def download():
+    link = link_field.get
     yt = YouTube(link)
     print("Title: ", yt.title)
     print("Views: ", yt.views)
@@ -28,42 +48,24 @@ def login():
 
 
 
-# adjusting frame settings
-frame.pack(pady=20, padx=2, fill="both", expand=True)
 
-# adding elements into frame
-label = customtkinter.CTkLabel(master=frame, text="Youtube Downloader", font=("Roboto", 24))
-label.pack(pady=12, padx=4)
+
 
 # adding text entries
-entry1 = customtkinter.CTkEntry(master=frame, width=400, placeholder_text="Enter video url:")
-entry1.pack(pady=2, padx=2)
+
 
 def combobox_callback(choice):
     print("combobox dropdown clicked:", choice)
 
-combobox_var = customtkinter.StringVar(value="MP3")  # set initial value
-combobox = customtkinter.CTkComboBox(master=frame,
-                                     values=['MP4', 'MP3'],
-                                     variable='combobox_var',
-                                    width=400,
-                                     )
-combobox['values'] = ('MP4', 'MP3')
-combobox.configure(values=['MP4', 'MP3'], variable='combobox_var')
-combobox.pack(pady=12, padx=10)
 
 
 
-button = customtkinter.CTkButton(master=frame, width=400, text="Download", command=login)
-button.pack(pady=12, padx=10)
+
+
 
 checkbox = customtkinter.CTkCheckBox(master=frame, text="Highest Quality")
 checkbox.pack(pady=12, padx=10)
 
 
-
-root.mainloop(
-
-
-
-)
+# run the main loop
+root.mainloop
